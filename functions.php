4429a22afee101983478fc5ba1d8dd9b48335f17
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-28 18:35:39 +0900

    update WordPress function
    load builded react scripts
    add wp_footer

--- functions.php
+++ functions.php
@@ -14,5 +14,6 @@ function oribe_scripts() {
 	wp_enqueue_script( 'oribe-react-browser', 'https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js', array(), '20151228' );
 	wp_deregister_script('jquery');
 	wp_enqueue_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js', array(), '20151228' );
+	wp_enqueue_script( 'oribe-scripts', get_template_directory_uri() .'/app.js' , array(), '20151228', true );
 }
 add_action( 'wp_enqueue_scripts', 'oribe_scripts' );
--- index.php
+++ index.php
@@ -7,20 +7,6 @@
 	</head>
 	<body <?php body_class() ;?> >
 		<div id="content"></div>
-		<script type="text/babel">
-			var CommentBox = React.createClass({
-			  render: function() {
-			    return (
-			      <div className="commentBox">
-			        Hello, world! I am a CommentBox.
-			      </div>
-			    );
-			  }
-			});
-			ReactDOM.render(
-			  <CommentBox />,
-			  document.getElementById('content')
-			);
-		</script>
+		<?php wp_footer();?>
 	</body>
 </html>
