7d9464340634c6a8e8b8b968344e0b02315e2f9c
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-28 17:42:50 +0900

    init react script

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function oribe_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'oribe_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'oribe_content_width', 0 );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function oribe_scripts() {
+	wp_enqueue_style( 'oribe-style', get_stylesheet_uri() );
+	wp_enqueue_script( 'oribe-react', 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.3/react.js', array(), '20151228' );
+	wp_enqueue_script( 'oribe-react-dom', 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.3/react-dom.js', array(), '20151228' );
+	wp_enqueue_script( 'oribe-react-browser', 'https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js', array(), '20151228' );
+	wp_deregister_script('jquery');
+	wp_enqueue_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js', array(), '20151228' );
+}
+add_action( 'wp_enqueue_scripts', 'oribe_scripts' );
--- index.php
+++ index.php
@@ -6,6 +6,21 @@
 		<?php wp_head();?>
 	</head>
 	<body <?php body_class() ;?> >
-		<div id="apps"></div>
+		<div id="content"></div>
+		<script type="text/babel">
+			var CommentBox = React.createClass({
+			  render: function() {
+			    return (
+			      <div className="commentBox">
+			        Hello, world! I am a CommentBox.
+			      </div>
+			    );
+			  }
+			});
+			ReactDOM.render(
+			  <CommentBox />,
+			  document.getElementById('content')
+			);
+		</script>
 	</body>
 </html>
