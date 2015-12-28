6df75bde43d26be7a694a843fcf58758006dc0df
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-28 17:01:19 +0900

    html base

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-echo "code is poet";
+<!DOCTYPE html>
+<html <?php language_attributes();?> >
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+		<?php wp_head();?>
+	</head>
+	<body <?php body_class() ;?> >
+		<div id="apps"></div>
+	</body>
+</html>
