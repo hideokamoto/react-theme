8b788cafb0c8a3feef005e45765f817e969c3cab
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-30 01:30:45 +0900

    create article class

--- modules/app.jsx
+++ modules/app.jsx
@@ -4,13 +4,27 @@ var ReactDOM = require('react-dom');
 
 // Init API URL
 var apiUrl = document.getElementById('content').dataset.siteUrl + '/wp-json/wp/v2/';
-var commentApiUrl = apiUrl + 'comments';
 
 // Load components
 var CommentBox = require('../modules/comment.jsx');
 
+// Class
+var Article = React.createClass({
+	getInitialState: function() {
+		return {data: []};
+	},
+	render: function() {
+		var commentApiUrl = this.props.apiUrl + 'comments';
+		return (
+			<div className="commentBox panel panel-default">
+				<CommentBox url={commentApiUrl} pollInterval={60000}/>
+			</div>
+		);
+	}
+});
+
 // Render
 ReactDOM.render(
-  <CommentBox url={commentApiUrl} pollInterval={60000}/>,
-  document.getElementById('content')
+	<Article apiUrl={apiUrl}/>,
+	document.getElementById('content')
 );
