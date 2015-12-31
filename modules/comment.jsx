5a3883a56b6b86489e803ab6ec6fff65dff3005f
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-31 13:34:53 +0900

    move commentList to new file

--- modules/comment.jsx
+++ modules/comment.jsx
@@ -1,40 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Comment = React.createClass({
-	render: function() {
-		return (
-			<li className="comment list-group-item">
-				<div className="panel-headin">
-					<h3 className="commentAuthor panel-titl">
-						{this.props.author}
-					</h3>
-				</div>
-				<div className="panel-bod">
-					{this.props.children}
-				</div>
-			</li>
-		);
-	}
-});
-
-var CommentList = React.createClass({
-	render: function() {
-		var commentNodes = this.props.data.map(function (comment) {
-			return (
-				<Comment author={comment.author_name} key={comment.id}>
-					<div dangerouslySetInnerHTML={{__html: comment.content.rendered}} />
-				</Comment>
-			);
-		});
-		return (
-			<ul className="commentList list-group">
-				{commentNodes}
-			</ul>
-		);
-	}
-});
-
+var CommentList = require('../modules/commentList.jsx');
 var CommentForm = React.createClass({
 	handleSubmit: function(e) {
 		e.preventDefault();
