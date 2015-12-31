bf2ea58cb863353e5a372d9fabae976ca14cc393
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-31 18:43:28 +0900

    remove scripts

--- functions.php
+++ functions.php
@@ -27,9 +27,6 @@ function oribe_nav_class($classes, $item){
 function oribe_scripts() {
 	wp_enqueue_style( 'oribe-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
 		wp_enqueue_style( 'oribe-style', get_stylesheet_uri() );
-	wp_enqueue_script( 'oribe-react', 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.3/react.js', array(), '20151228' );
-	wp_enqueue_script( 'oribe-react-dom', 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.3/react-dom.js', array(), '20151228' );
-	wp_enqueue_script( 'oribe-react-browser', 'https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js', array(), '20151228' );
 	wp_deregister_script('jquery');
 	wp_enqueue_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js', array(), '20151228' );
 	wp_enqueue_script( 'oribe-scripts', get_template_directory_uri() .'/app.js' , array(), '20151228', true );
